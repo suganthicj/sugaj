55cfcfed258638aec479651b25d7b5cf291082b3
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-05 14:51:26 +0530

    Create sugaj.py

---
+++ sugaj.py
@@ -0,0 +1,8 @@
+def median(lst):
+    n = len(lst)
+    if n < 1:
+            return None
+    if n % 2 == 1:
+            return sorted(lst)[n//2]
+    else:
+            return sum(sorted(lst)[n//2-1:n//2+1])/2.0
